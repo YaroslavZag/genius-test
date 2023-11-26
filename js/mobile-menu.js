5c56d2744dcb78b91c69da79738351707580002c
YaroslavZag <yarikzag2005@gmail.com>
2023-11-26 12:46:02 +0200

    update  project

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,13 @@
-const MobileMenu = document.querySelector(".mobile-menu");
-const menuBtnOpen = document.querySelector(".mobile-btn-open");
-const menuBtnClose = document.querySelector(".mobile-btn-close");
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
 
-const toggleMenu = () => MobileMenu.classList.toggle("is-open");
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const disableScroll = () =>
+  document.body.classList.toggle('is-scroll-disabled');
 
-menuBtnOpen.addEventListener("click", toggleMenu);
-menuBtnClose.addEventListener("click", toggleMenu);
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
+
+menuBtnOpen.addEventListener('click', disableScroll);
+menuBtnClose.addEventListener('click', disableScroll);
